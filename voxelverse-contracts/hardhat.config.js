19b2452e66c876b15389f8210427fab405107a9a
DJe lakhdar <lakhdar.dj.2004@gmail.com>
2025-07-09 20:04:09 +0200

    Update deploy.js for ethers v6 and adjust network config

--- voxelverse-contracts/hardhat.config.js
+++ voxelverse-contracts/hardhat.config.js
@@ -1,3 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: "https://polygon-amoy.g.alchemy.com/v2/ToS-yKnpawn1ORRbXbMYB",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
